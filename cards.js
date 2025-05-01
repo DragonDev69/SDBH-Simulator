c06fa02c6414c494ea4c9fbeb0f8ecc449621594
DragonDev69 <davidjnoel9@gmail.com>
2025-04-30 21:06:37 -0400

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,5 @@
+
+const cards = [
+  { name: "Goku", stamina: 100, power: 30, super: 60, image: "images/goku_card.png" },
+  { name: "Vegeta", stamina: 100, power: 28, super: 58, image: "images/vegeta_card.png" }
+];
---
+++ game.js
@@ -0,0 +1,6 @@
+
+function startGame() {
+  document.getElementById('main-menu').style.display = 'none';
+  document.getElementById('game-screen').style.display = 'block';
+  document.getElementById('battle-log').innerText = 'Game Started!';
+}
